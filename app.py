181a3cba8f2f653916eff7a98bfe95eb9f1bad4e
Pavlova Liliia <imperial1511@ukr.net>
2024-10-21 17:24:11 +0300

    add divide

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
 def add(x, y):
     return x + y
-
 def subtract(x, y):
     return x - y
+def multiply(x, y):
+    return x * y
+def divide(x, y):
+    if y == 0:
+        return "Error!"
+    return x / y
 
 print("select an action")
 print("add")
 print("subtraction")
+print("multiply")
+print("divide")
 
 choice = input("select an action: ")
 
 num1 = int(input("add: "))
 num2 = int(input("subtraction: "))
 
+
 if choice == '1':
     print(f"res: {num1} + {num2} = {add(num1, num2)}")
 elif choice == '2':
     print(f"res: {num1} - {num2} = {subtract(num1, num2)}")
+elif choice == '3':
+    print(f"res: {num1} * {num2} = {multiply(num1, num2)}")
+elif choice == '4':
+    print(f"res: {num1} / {num2} = {divide(num1, num2)}")
 else:
     print("entered incorrectly!")
